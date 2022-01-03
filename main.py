d14014f589ba99a97231018f7657317e2349e07e
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-03 23:40:59 +0000

    moving code into function ~ alphabet_gen()

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_data_list = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 print(nato_data_list)
 
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-game_on = True
-while game_on:
+def alphabet_gen():
     try:
         user_word = input("Enter a word: ").upper()
         user_nato_list = [nato_data_list[letter] for letter in user_word]
     except KeyError:
         print("Sorry, only letters in the alphabet please.")
+        alphabet_gen()
     else:
         print(user_nato_list)
-    finally:
-        more_word = input("More word? 'y' or 'n' ").lower()
-        if more_word == "n":
-            print("Thank you, bye!")
-            game_on = False
+
+
+game_on = True
+while game_on:
+    alphabet_gen()
+    more_word = input("More word? 'y' or 'n' ").lower()
+    if more_word == "n":
+        print("Thank you, bye!")
+        game_on = False
